130133a5d15bd8dc2b322cab5598790d58d41c06
Aqib Qayyum <aqib.qayyum@virtual-force.com>
2020-01-31 18:36:16 +0500

    update app with redirect

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,7 +48,7 @@ class Directory extends Component {
 
   render() {
     return (
-      <div class='directory-menu'>
+      <div className='directory-menu'>
         {
           this.state.sections.map(({id, ...otherSectionProps }) => (
             <MenuItem key={id} {...otherSectionProps} />
